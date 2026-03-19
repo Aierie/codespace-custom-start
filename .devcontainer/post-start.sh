3184576186989cc0b511b12b0ccd04ca7a359ce7
Aierie <39579264+Aierie@users.noreply.github.com>
2026-03-19 11:03:02 +0000

    more tries

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -5,6 +5,9 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 FLAG_DIR="$ROOT_DIR/.vscode"
 FLAG_FILE="$FLAG_DIR/open-custom-screen"
 
+npm run package:vsix                                                        
+code --install-extension ./custom-screen-extension-0.0.1.vsix --force
+
 mkdir -p "$FLAG_DIR"
 touch "$FLAG_FILE"
 
